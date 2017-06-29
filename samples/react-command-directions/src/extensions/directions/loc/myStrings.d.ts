1d353a7d08d67d4bc87b3e467081285c10227ede
Waldek Mastykarz <waldek@mastykarz.nl>
2017-06-29 19:46:59 +0200

    Added directions list view command set sample

---
+++ samples/react-command-directions/src/extensions/directions/loc/myStrings.d.ts
@@ -0,0 +1,9 @@
+declare interface IDrivingDirectionsStrings {
+  Command1: string;
+  Command2: string;
+}
+
+declare module 'drivingDirectionsStrings' {
+  const strings: IDrivingDirectionsStrings;
+  export = strings;
+}
