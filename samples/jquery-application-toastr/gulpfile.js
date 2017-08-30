c15826a874a70e3bee78810067e4c53a2b90f40d
thechriskent <ihatetimemachines@gmail.com>
2017-08-30 14:29:39 -0400

    Updated Toastr to Release Candidate

--- samples/jquery-application-toastr/gulpfile.js
+++ samples/jquery-application-toastr/gulpfile.js
@@ -3,8 +3,4 @@
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 
-require('./gulpfile-serve-info');
-
 build.initialize(gulp);
-
-gulp.tasks['serve-info'].dep.push('serve');
