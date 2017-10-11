4e42dd4f434b04df21abc37e68cda7a6444d5560
paolo@pialorsi.com <paolo@pialorsi.com>
2017-10-11 12:01:02 +0200

    Figures and scripts for Regions Footer

--- samples/react-application-regions-footer/RegionsFooterProvisionCustomizer.ps1
+++ samples/react-application-regions-footer/RegionsFooterProvisionCustomizer.ps1
@@ -1,5 +1,5 @@
 ﻿$credentials = Get-Credential
-Connect-PnPOnline "https://<your-tenant>.sharepoint.com/sites/travelinnovation" -Credentials $credentials
+Connect-PnPOnline "https://<your-tenant>.sharepoint.com/sites/<your-site>" -Credentials $credentials
 
 $context = Get-PnPContext
 $web = Get-PnPWeb
@@ -10,9 +10,9 @@ $ca = $web.UserCustomActions.Add()
 $ca.ClientSideComponentId = "67fd1d01-84e8-4fbf-85bd-4b80768c6080"
 $ca.ClientSideComponentProperties = "{""SourceTermSetName"":""Regions""}"
 $ca.Location = "ClientSideExtension.ApplicationCustomizer"
-$ca.Name = "YammerFooterCustomAction"
-$ca.Title = "YammerFooterCustomizer"
-$ca.Description = "Custom action for Yammer Footer Application Customizer"
+$ca.Name = "RegionsFooterCustomAction"
+$ca.Title = "RegionsFooterCustomizer"
+$ca.Description = "Custom action for Regions Footer Application Customizer"
 $ca.Update()
 
 $context.Load($web.UserCustomActions)
