a5d630ab9eff761db8914163fd972e329d095538
Anoop Tatti <anoop.tatti@gmail.com>
2017-12-05 13:49:16 +0000

    Removed URL from update

--- samples/react-application-myfavourites/src/extensions/services/MyFavouritesService.ts
+++ samples/react-application-myfavourites/src/extensions/services/MyFavouritesService.ts
@@ -71,8 +71,7 @@ export class MyFavouritesService implements IMyFavoutitesService {
     public async updateFavourite(favouriteItem: IMyFavouriteItem): Promise<boolean> {
         return pnp.sp.web.lists.getByTitle(FAVOURITES_LIST_NAME).items.getById(favouriteItem.Id).update({
             'Title': favouriteItem.Title,
-            'Description': favouriteItem.Description,
-            'ItemUrl': window.location.href
+            'Description': favouriteItem.Description
         }).then(async (result: ItemUpdateResult): Promise<boolean> => {
             console.log(result);
             await this.getMyFavourites(false);
