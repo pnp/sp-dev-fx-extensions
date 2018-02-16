109c3e991974d24d465c593c194c6f199f3260b8
luismanez <luis.manez@outlook.com>
2018-02-16 17:05:42 +0100

    Sample added

---
+++ samples/react-application-grouplinks/src/extensions/groupDirectLinks/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IGroupDirectLinksApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'GroupDirectLinksApplicationCustomizerStrings' {
+  const strings: IGroupDirectLinksApplicationCustomizerStrings;
+  export = strings;
+}
