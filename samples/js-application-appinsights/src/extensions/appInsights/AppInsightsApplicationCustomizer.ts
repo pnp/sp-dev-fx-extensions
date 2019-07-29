5413566b7b1a7bf2cb18391ffd18266100c34a21
Guillaume Sellier <guisell@microsoft.com>
2019-07-29 19:52:12 +0200

    Updated the sample to make it work with SPFx 1.8.2

---
+++ samples/js-application-appinsights/src/extensions/appInsights/AppInsightsApplicationCustomizer.ts
@@ -0,0 +1,42 @@
+import { override } from '@microsoft/decorators';
+import { Log } from '@microsoft/sp-core-library';
+import {
+  BaseApplicationCustomizer
+} from '@microsoft/sp-application-base';
+import { Dialog } from '@microsoft/sp-dialog';
+
+import * as strings from 'AppInsightsApplicationCustomizerStrings';
+
+const LOG_SOURCE: string = 'AppInsightsApplicationCustomizer';
+
+export interface IAppInsightsApplicationCustomizerProperties {
+  
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class AppInsightsApplicationCustomizer
+  extends BaseApplicationCustomizer<IAppInsightsApplicationCustomizerProperties> {
+
+  @override
+  public onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+
+    // Update this to your Azure Application Insights Instrumentation Key
+    let appInsightsKey = "a09f32d3-479e-4782-b6db-1bdce04536db";
+
+    let appInsightsScript: string = `var appInsights=window.appInsights||function(a){
+  function b(a){c[a]=function(){var b=arguments;c.queue.push(function(){c[a].apply(c,b)})}}var c={config:a},d=document,e=window;setTimeout(function(){var b=d.createElement("script");b.src=a.url||"https://az416426.vo.msecnd.net/scripts/a/ai.0.js",d.getElementsByTagName("script")[0].parentNode.appendChild(b)});try{c.cookie=d.cookie}catch(a){}c.queue=[];for(var f=["Event","Exception","Metric","PageView","Trace","Dependency"];f.length;)b("track"+f.pop());if(b("setAuthenticatedUserContext"),b("clearAuthenticatedUserContext"),b("startTrackEvent"),b("stopTrackEvent"),b("startTrackPage"),b("stopTrackPage"),b("flush"),!a.disableExceptionTracking){f="onerror",b("_"+f);var g=e[f];e[f]=function(a,b,d,e,h){var i=g&&g(a,b,d,e,h);return!0!==i&&c["_"+f](a,b,d,e,h),i}}return c
+    }({
+      instrumentationKey: "` + appInsightsKey + `"
+    });
+  window.appInsights=appInsights,appInsights.queue&&0===appInsights.queue.length&&appInsights.trackPageView();`;
+
+    let script = document.createElement("script");
+    script.type = "text/javascript";
+    script.appendChild(document.createTextNode(appInsightsScript));
+
+    document.head.insertAdjacentElement("beforeend", script);
+
+    return Promise.resolve();
+  }
+}
