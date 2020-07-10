879f9ddd96b701387d15bfc7d36296e7d82f3905
Sudharsan K <10280385+sudharsank@users.noreply.github.com>
2020-07-10 12:53:17 +0800

    Configured few properties and also added the codetour for solution walkthrough

--- samples/js-application-alert-message/src/extensions/globalAlerts/GlobalAlertsApplicationCustomizer.ts
+++ samples/js-application-alert-message/src/extensions/globalAlerts/GlobalAlertsApplicationCustomizer.ts
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { override } from '@microsoft/decorators';
 import { Log } from '@microsoft/sp-core-library';
 import {
-	BaseApplicationCustomizer, PlaceholderName, PlaceholderContent
+    BaseApplicationCustomizer, PlaceholderName, PlaceholderContent
 } from '@microsoft/sp-application-base';
 import * as strings from 'GlobalAlertsApplicationCustomizerStrings';
 import { sp } from "@pnp/sp";
@@ -17,40 +17,48 @@ const LOG_SOURCE: string = 'GlobalAlertsApplicationCustomizer';
  * You can define an interface to describe it.
  */
 export interface IGlobalAlertsApplicationCustomizerProperties {
-	// This is an example; replace with your own property
-	animationType: string;
+    // This is an example; replace with your own property
+    animationType: string;
+    animationDelay: number;
+    alertBackgroundColor: string;
+    textColor: string;
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
 export default class GlobalAlertsApplicationCustomizer
-	extends BaseApplicationCustomizer<IGlobalAlertsApplicationCustomizerProperties> {
-	private _topPlaceholder: PlaceholderContent | undefined;
-	private _renderPlaceHolders(): void {
-		// Handling the top placeholder
-		if (!this._topPlaceholder) {
-			this._topPlaceholder = this.context.placeholderProvider.tryCreateContent(
-				PlaceholderName.Top,
-				{ onDispose: this._onDispose }
-			);
-			// The extension should not assume that the expected placeholder is available.
-			if (!this._topPlaceholder) {
-				console.error("The expected placeholder (Top) was not found.");
-				return;
-			}
-			if (this._topPlaceholder.domElement) {
-				const elem: React.ReactElement<IAlertsProps> = React.createElement(Alerts, { animationType: this.properties.animationType });
-				ReactDOM.render(elem, this._topPlaceholder.domElement);
-			}
-		}
-	}
-	private _onDispose(): void {
-		console.log('[GlobalAlertApplicationCustomizer._onDispose] Disposed custom top placeholders.');
-	}
-	@override
-	public onInit(): Promise<void> {
-		sp.setup(this.context);
-		// Wait for the placeholders to be created (or handle them being changed) and then render.
-		this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
-		return Promise.resolve();
-	}
+    extends BaseApplicationCustomizer<IGlobalAlertsApplicationCustomizerProperties> {
+    private _topPlaceholder: PlaceholderContent | undefined;
+    private _renderPlaceHolders(): void {
+        // Handling the top placeholder
+        if (!this._topPlaceholder) {
+            this._topPlaceholder = this.context.placeholderProvider.tryCreateContent(
+                PlaceholderName.Top,
+                { onDispose: this._onDispose }
+            );
+            // The extension should not assume that the expected placeholder is available.
+            if (!this._topPlaceholder) {
+                console.error("The expected placeholder (Top) was not found.");
+                return;
+            }
+            if (this._topPlaceholder.domElement) {
+                const elem: React.ReactElement<IAlertsProps> = React.createElement(Alerts, {
+                    animationType: this.properties.animationType,
+                    animationDelay: this.properties.animationDelay,
+                    alertBackgroundColor: this.properties.alertBackgroundColor,
+                    textColor: this.properties.textColor
+                });
+                ReactDOM.render(elem, this._topPlaceholder.domElement);
+            }
+        }
+    }
+    private _onDispose(): void {
+        console.log('[GlobalAlertApplicationCustomizer._onDispose] Disposed custom top placeholders.');
+    }
+    @override
+    public onInit(): Promise<void> {
+        sp.setup(this.context);
+        // Wait for the placeholders to be created (or handle them being changed) and then render.
+        this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
+        return Promise.resolve();
+    }
 }
