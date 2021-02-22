b9f5be9dee9a22c689ea22a190d1dd1134c47b71
Mikael Svenson <misvenso@microsoft.com>
2021-02-22 13:16:36 +0100

    Delete samples/react-application-breadcrumb/lib directory

--- samples/react-application-breadcrumb/lib/SiteBreadcrumb.module.scss.js
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-require("./SiteBreadcrumb.module.css");
-var styles = {
-    breadcrumb: 'breadcrumb_65d757c0',
-    breadcrumbLinks: 'breadcrumbLinks_65d757c0'
-};
-export default styles;
-/* tslint:enable */ 
-//# sourceMappingURL=SiteBreadcrumb.module.scss.js.map
