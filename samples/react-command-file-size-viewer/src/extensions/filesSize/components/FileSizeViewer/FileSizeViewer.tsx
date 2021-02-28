19df1556da5e39c6d31d68d15d782b9f6e773cf2
Sudharsan K <10280385+sudharsank@users.noreply.github.com>
2021-02-28 22:01:55 +0800

    Version upgrade and fix for #360

--- samples/react-command-file-size-viewer/src/extensions/filesSize/components/FileSizeViewer/FileSizeViewer.tsx
+++ samples/react-command-file-size-viewer/src/extensions/filesSize/components/FileSizeViewer/FileSizeViewer.tsx
@@ -1,32 +1,34 @@
 import * as React from 'react';
 
 import TreeMap, { ColorModel } from "react-d3-treemap";
+import { DialogContent } from 'office-ui-fabric-react/lib/Dialog';
 // Be sure to include styles at some point
 import "react-d3-treemap/dist/react.d3.treemap.css";
 
-import { IFileSizeViewerProps } from "./IFileSizeViewerProps";
-
-export class FileSizeViewer extends React.Component<IFileSizeViewerProps, {}> {
-
-  constructor(props) {
-    super(props);
-  }
-
-  public render(): JSX.Element {
-    return (
-      <TreeMap
-        height={300}
-        width={600}
-        data={this.props.data}
-        valueUnit={"KB"}
-        colorModel={ColorModel.Depth}
-        disableBreadcrumb={true}
-        bgColorRangeLow={"#757575"}
-        bgColorRangeHigh={"#33ccff"}
-      />
-    );
-  }
-
+export interface IFileSizeViewerProps {
+    data: any;
+    close: () => void;
 }
 
+const FileSizeViewer: React.FC<IFileSizeViewerProps> = (props) => {
+    return (
+        <DialogContent
+            title="File Size Viewer"
+            showCloseButton={true}
+            onDismiss={props.close}
+        >
+            <TreeMap
+                height={300}
+                width={600}
+                data={props.data}
+                valueUnit={"KB"}
+                colorModel={ColorModel.Depth}
+                disableBreadcrumb={false}
+                bgColorRangeLow={"#757575"}
+                bgColorRangeHigh={"#33ccff"}
+            />
+        </DialogContent>
+    );
+};
 
+export default FileSizeViewer;
--- samples/react-command-file-size-viewer/src/extensions/filesSize/data/data.ts
+++ samples/react-command-file-size-viewer/src/extensions/filesSize/data/data.ts
@@ -1,9 +1,32 @@
 export const data = {
-  "name": "Documents",
-  "children": [
-    { "name": "Document 1", "value": 3938 },
-    { "name": "Document 2", "value": 3812 },
-    { "name": "Document 3", "value": 6714 },
-    { "name": "Document 4", "value": 743 }
-  ]
+    "name": "Documents",
+    children: [
+        {
+            name: "cluster",
+            children: [
+                {
+                    name: "AgglomerativeCluster",
+                    value: 3938,
+                    link: "https://blog.josequinto.com"
+                },
+                { name: "CommunityStructure", value: 3812 },
+                { name: "HierarchicalCluster", value: 6714 },
+                { name: "MergeEdge", value: 743 }
+            ]
+        },
+        {
+            name: "graph",
+            children: [
+                { name: "BetweennessCentrality", value: 3534 },
+                { name: "LinkDistance", value: 5731 },
+                { name: "MaxFlowMinCut", value: 7840 },
+                { name: "ShortestPaths", value: 5914 },
+                { name: "SpanningTree", value: 3416 }
+            ]
+        },
+        {
+            name: "optimization",
+            children: [{ name: "AspectRatioBanker", value: 7074 }]
+        }
+    ]
 };
