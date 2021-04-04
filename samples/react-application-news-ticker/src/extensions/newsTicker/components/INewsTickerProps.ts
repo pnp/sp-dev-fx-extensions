8ff60c38776c41ff8b7089492ae577e9b52f1def
Ari Gunawan <ari.gunawan.3023@gmail.com>
2021-04-04 20:44:33 +0700

    Get data from list

---
+++ samples/react-application-news-ticker/src/extensions/newsTicker/components/INewsTickerProps.ts
@@ -0,0 +1,5 @@
+import { News } from "../models/News";
+
+export default interface INewsTickerProps {
+  items: News[];
+}
---
+++ samples/react-application-news-ticker/src/extensions/newsTicker/models/News.ts
@@ -0,0 +1,5 @@
+export type News = {
+  title: string;
+  content: string;
+  publishDate: Date;
+}
