a3bdcff8061436a00daec1139690fb6b5e1b3f78
AriGunawan <ari.gunawan.3023@gmail.com>
2021-10-09 18:42:01 +0700

    Add initial code

---
+++ samples/js-field-animated-progress/src/extensions/animatedProgress/AnimatedProgressFieldCustomizer.ts
@@ -0,0 +1,53 @@
+import { Log } from '@microsoft/sp-core-library';
+import { override } from '@microsoft/decorators';
+import {
+  BaseFieldCustomizer,
+  IFieldCustomizerCellEventParameters
+} from '@microsoft/sp-listview-extensibility';
+
+import * as strings from 'AnimatedProgressFieldCustomizerStrings';
+import styles from './AnimatedProgressFieldCustomizer.module.scss';
+
+/**
+ * If your field customizer uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface IAnimatedProgressFieldCustomizerProperties {
+  // This is an example; replace with your own property
+  sampleText?: string;
+}
+
+const LOG_SOURCE: string = 'AnimatedProgressFieldCustomizer';
+
+export default class AnimatedProgressFieldCustomizer
+  extends BaseFieldCustomizer<IAnimatedProgressFieldCustomizerProperties> {
+
+  @override
+  public onInit(): Promise<void> {
+    // Add your custom initialization to this method.  The framework will wait
+    // for the returned promise to resolve before firing any BaseFieldCustomizer events.
+    Log.info(LOG_SOURCE, 'Activated AnimatedProgressFieldCustomizer with properties:');
+    Log.info(LOG_SOURCE, JSON.stringify(this.properties, undefined, 2));
+    Log.info(LOG_SOURCE, `The following string should be equal: "AnimatedProgressFieldCustomizer" and "${strings.Title}"`);
+    return Promise.resolve();
+  }
+
+  @override
+  public onRenderCell(event: IFieldCustomizerCellEventParameters): void {
+    // Use this method to perform your custom cell rendering.
+    const text: string = `${this.properties.sampleText}: ${event.fieldValue}`;
+
+    event.domElement.innerText = text;
+
+    event.domElement.classList.add(styles.cell);
+  }
+
+  @override
+  public onDisposeCell(event: IFieldCustomizerCellEventParameters): void {
+    // This method should be used to free any resources that were allocated during rendering.
+    // For example, if your onRenderCell() called ReactDOM.render(), then you should
+    // call ReactDOM.unmountComponentAtNode() here.
+    super.onDisposeCell(event);
+  }
+}
