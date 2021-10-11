09058dd0cc3ffe6f79f3772bab3394a2e32742fc
AriGunawan <ari.gunawan.3023@gmail.com>
2021-10-11 21:57:24 +0700

    Implement animated progress

--- samples/js-field-animated-progress/src/extensions/animatedProgress/AnimatedProgressFieldCustomizer.ts
+++ samples/js-field-animated-progress/src/extensions/animatedProgress/AnimatedProgressFieldCustomizer.ts
@@ -2,7 +2,7 @@ import { Log } from '@microsoft/sp-core-library';
 import { override } from '@microsoft/decorators';
 import {
   BaseFieldCustomizer,
-  IFieldCustomizerCellEventParameters
+  IFieldCustomizerCellEventParameters,
 } from '@microsoft/sp-listview-extensibility';
 
 import * as strings from 'AnimatedProgressFieldCustomizerStrings';
@@ -20,27 +20,70 @@ export interface IAnimatedProgressFieldCustomizerProperties {
 
 const LOG_SOURCE: string = 'AnimatedProgressFieldCustomizer';
 
-export default class AnimatedProgressFieldCustomizer
-  extends BaseFieldCustomizer<IAnimatedProgressFieldCustomizerProperties> {
+export default class AnimatedProgressFieldCustomizer extends BaseFieldCustomizer<IAnimatedProgressFieldCustomizerProperties> {
+  private readonly _maxLowLevel = 0.3;
+  private readonly _maxMiddleLevel = 0.7;
+  private readonly _baseId = 'animated-progress-';
+  private initializedCells = [];
 
   @override
   public onInit(): Promise<void> {
     // Add your custom initialization to this method.  The framework will wait
     // for the returned promise to resolve before firing any BaseFieldCustomizer events.
-    Log.info(LOG_SOURCE, 'Activated AnimatedProgressFieldCustomizer with properties:');
+    Log.info(
+      LOG_SOURCE,
+      'Activated AnimatedProgressFieldCustomizer with properties:'
+    );
     Log.info(LOG_SOURCE, JSON.stringify(this.properties, undefined, 2));
-    Log.info(LOG_SOURCE, `The following string should be equal: "AnimatedProgressFieldCustomizer" and "${strings.Title}"`);
+    Log.info(
+      LOG_SOURCE,
+      `The following string should be equal: "AnimatedProgressFieldCustomizer" and "${strings.Title}"`
+    );
     return Promise.resolve();
   }
 
   @override
   public onRenderCell(event: IFieldCustomizerCellEventParameters): void {
-    // Use this method to perform your custom cell rendering.
-    const text: string = `${this.properties.sampleText}: ${event.fieldValue}`;
+    // Get percentage value
+    const percentage = event.fieldValue * 100;
 
-    event.domElement.innerText = text;
+    // Determine the type: low/middle/top
+    let typeClass;
+    if (event.fieldValue <= this._maxLowLevel) {
+      typeClass = styles.lowLevel;
+    } else if (event.fieldValue <= this._maxMiddleLevel) {
+      typeClass = styles.middleLevel;
+    } else {
+      typeClass = styles.topLevel;
+    }
 
-    event.domElement.classList.add(styles.cell);
+    // Generate uinque id for element
+    const id = this._baseId + event.listItem.getValueByName('ID');
+    event.domElement.innerHTML = `
+    <div id="${id}" class="${styles.progress} ${typeClass}">
+      <span style="width: ${percentage}%">${percentage}%</span>
+    </div>
+    `;
+
+    const progressBar = event.domElement.getElementsByTagName('span')[0];
+    if (this.initializedCells.indexOf(id) == -1) {
+      // Animate the initial progress grow
+      progressBar.animate(
+        [
+          // keyframes
+          { width: 0 },
+          { width: percentage + '%' },
+        ],
+        {
+          // timing options
+          duration: 1000,
+          iterations: 1,
+        }
+      );
+      this.initializedCells.push(id);
+    } else {
+      progressBar.style.width = percentage + '%';
+    }
   }
 
   @override
