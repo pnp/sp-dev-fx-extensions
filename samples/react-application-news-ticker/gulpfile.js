0eae0dea699a7e049f397adc2772dc9864752a0d
AriGunawan <ari.gunawan.3023@gmail.com>
2021-10-17 07:36:47 +0700

    Update spfx version

--- samples/react-application-news-ticker/gulpfile.js
+++ samples/react-application-news-ticker/gulpfile.js
@@ -32,5 +32,14 @@ if (useCustomServe) {
 
 }
 
+var getTasks = build.rig.getTasks;
+build.rig.getTasks = function () {
+  var result = getTasks.call(build.rig);
+
+  result.set('serve', result.get('serve-deprecated'));
+
+  return result;
+};
+
 build.initialize(require('gulp'));
 
