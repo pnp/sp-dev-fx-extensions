713d9b2c4bbaebea0df952d1f1c9a7399e6a4787
Sandeep-FED <sandymankara11@gmail.com>
2023-07-05 14:19:20 +0530

    init: react feedback widget application customizer
    Signed-off-by: Sandeep-FED <sandymankara11@gmail.com>

---
+++ samples/react-application-customizer-feedback-widget/src/extensions/feedback/components/SuccessPage.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import styles from "./FeedbackCustomizer.module.scss";
+import Lottie from "lottie-react";
+import Success from "../../assets/success-animation.json";
+
+export interface ISuccessProps {
+  goBack:()=>void;
+}
+
+export const SuccessPage: React.FC<ISuccessProps> = (props:ISuccessProps): JSX.Element => {
+
+  const handleGoBack = () => {
+    props.goBack && props.goBack();
+  };
+
+  return (
+    <div className={styles["successWrapper"]}>
+      <p style={{ marginBottom: "0", fontSize: "15px", fontWeight: "500" }}>
+        Thanks for submitting your feedback!
+      </p>
+      <Lottie animationData={Success} style={{ width: "40%" }} loop={false} />
+      <p style={{ marginBottom: "0", marginTop: "0" }}>
+        We'll respond back to you shortly.
+      </p>
+      <p style={{ marginBottom: "0", color: "#312783" }}>
+        Want to submit another response?
+      </p>
+      <p
+        style={{
+          marginBottom: "0",
+          marginTop: "0",
+          color: "#312783",
+          fontWeight: "700",
+          cursor: "pointer",
+        }}
+        onClick={handleGoBack}
+      >
+        Click here
+      </p>
+    </div>
+  )
+};
