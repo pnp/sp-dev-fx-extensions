1c75d1a2f3a5d7123fe641374718e7a204e6f875
Nishkalank <nishkalankbezawada@outlook.com>
2024-10-10 19:25:43 +0200

    Second incremental upgrade from SPFx v1.11.0 to SPFx v1.13.0

--- samples/react-command-print/src/extensions/print/components/print-dialog.tsx
+++ samples/react-command-print/src/extensions/print/components/print-dialog.tsx
@@ -3,22 +3,32 @@ import * as ReactDOM from 'react-dom';
 import { BaseDialog } from '@microsoft/sp-dialog';
 import PrintDialogContent from './print-dialog-content/print-dialog-content';
 
-class PrintDialog extends BaseDialog {    
+class PrintDialog extends BaseDialog {
+    close: () => void;
+    domElement: HTMLElement;
     public webUrl: string;
     public listId: string;
     public itemId: number;
     public title: string;
-    public render(): void {        
-        ReactDOM.render(<PrintDialogContent
-            close={this.close}
-            webUrl={this.webUrl}
-            listId={this.listId}
-            itemId={this.itemId}
-            title={this.title}
-        />, this.domElement);
+
+    public render(): void {
+        // Render the React component into the dialog
+        ReactDOM.render(
+            <PrintDialogContent
+                close={this.close}
+                webUrl={this.webUrl}
+                listId={this.listId}
+                itemId={this.itemId}
+                title={this.title}
+            />,
+            this.domElement as HTMLElement
+        );
+    }
+
+    // Override show() if needed, but it should be available in BaseDialog
+    public show(): Promise<void> {
+        return super.show();
     }
 }
 
-export{
-    PrintDialog
-};
+export { PrintDialog };
