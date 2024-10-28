73d65824d06de7eff895ddc1e0148a3fb4ac6e65
Thomas Daly <tom.m.daly@gmail.com>
2024-10-28 18:49:25 -0400

    Upgrade react-application-breadcrumb to SPFx v1.20.0 (#1419)
    * upgrade to spfx v1.20.0
    
    * Delete samples/react-application-breadcrumb/upgrade-report.md
    
    * Create sample.json
    
    add back the samples file
    
    * Add files via upload
    
    * Delete samples/react-application-breadcrumb/build.cmd
    
    remove windows build file
    
    * fix issue with the strings file

--- samples/react-application-breadcrumb/gulpfile.js
+++ samples/react-application-breadcrumb/gulpfile.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
+
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
-require('./gulpfile-serve-info');
 
-build.initialize(gulp);
+var getTasks = build.rig.getTasks;
+build.rig.getTasks = function () {
+  var result = getTasks.call(build.rig);
 
-gulp.tasks['serve-info'].dep.push('serve');
+  result.set('serve', result.get('serve-deprecated'));
+
+  return result;
+};
+
+build.initialize(require('gulp'));
